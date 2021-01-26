cdf82b5ab79d6898698e57a95bfa9b71f911ce19
Dan921 <57538438+Dan921@users.noreply.github.com>
2021-01-27 01:33:07 +0300

    changed BooksContext

--- Data/BooksContext.cs
+++ Data/BooksContext.cs
@@ -8,6 +8,10 @@ namespace Data
 {
     public class BooksContext : DbContext
     {
+        public BooksContext(DbContextOptions<BooksContext> options) : base(options)
+        {
+        }
+
         public DbSet<Book> Books { get; set; }
     }
 }
