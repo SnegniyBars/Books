cd204041827e509d517d4b573415bb2c87005ad4
Dan921 <57538438+Dan921@users.noreply.github.com>
2021-01-27 01:12:42 +0300

    added class Book

---
+++ Data/Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Entities
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public byte[] CoverImage { get; set; }
+        public int PublishingYear { get; set; }
+        public string Author { get; set; }
+    }
+}
