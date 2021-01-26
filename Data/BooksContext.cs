efd1788d71aab8f8946357d0092f015d787848ca
Dan921 <57538438+Dan921@users.noreply.github.com>
2021-01-27 01:14:04 +0300

    added BooksContext

---
+++ Data/BooksContext.cs
@@ -0,0 +1,13 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class BooksContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
