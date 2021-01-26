af62706310527fa327466e23da39e32d57b72b6c
Dan921 <57538438+Dan921@users.noreply.github.com>
2021-01-27 01:15:00 +0300

    added BookRepository

---
+++ Application/DAL/BookRepository.cs
@@ -0,0 +1,71 @@
+﻿using Data;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Application.DAL
+{
+    public class BookRepository : IBookRepository, IDisposable
+    {
+        private BooksContext context;
+
+        public BookRepository(BooksContext context)
+        {
+            this.context = context;
+        }
+
+        public void DeleteBook(int bookId)
+        {
+            Book book = context.Books.Find(bookId);
+            context.Books.Remove(book);
+        }
+
+        public Book GetBookByID(int bookId)
+        {
+            return context.Books.Find(bookId);
+        }
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return context.Books.ToList();
+        }
+
+        public void InsertBook(Book book)
+        {
+            context.Books.Add(book);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void UpdateBook(Book book)
+        {
+            context.Entry(book).State = EntityState.Modified;
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Application/DAL/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Application.DAL
+{
+    public interface IBookRepository : IDisposable
+    {
+        IEnumerable<Book> GetBooks();
+        Book GetBookByID(int bookId);
+        void InsertBook(Book book);
+        void DeleteBook(int bookId);
+        void UpdateBook(Book book);
+        void Save();
+    }
+}
