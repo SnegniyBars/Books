e10f37ebcb02e674911e6608b01c62039e1b018c
Dan921 <57538438+Dan921@users.noreply.github.com>
2021-01-27 01:18:01 +0300

    added BookDTO

---
+++ BooksWebAPI/Models/BookDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksWebAPI.Models
+{
+    public class BookDTO
+    {
+        public int BookId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public byte[] CoverImage { get; set; }
+        public int PublishingYear { get; set; }
+        public string Author { get; set; }
+    }
+}
